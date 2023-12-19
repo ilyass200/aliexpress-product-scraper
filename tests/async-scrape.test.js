4b1e6b0dfa802846c20059912fd80bca65715f2d
ajdaini ilyass <ajdainii@gmail.com>
2023-12-19 23:34:44 +0100

    clean comments

--- tests/async-scrape.test.js
+++ tests/async-scrape.test.js
@@ -5,7 +5,7 @@ const yargs = require('yargs');
 const argv = yargs
   .option('queries', {
     alias: 'q',
-    describe: 'Nombre de tests à effectuer',
+    describe: 'Number of tests to be performed',
     type: 'number',
     demandOption: true,
   })
@@ -13,24 +13,22 @@ const argv = yargs
     alias: 't',
     describe: 'Timeout for each scraping request in milliseconds',
     type: 'number',
-    default: 5000, // Set your default timeout value here
+    default: 5000,
   })
   .argv;
 
 describe('Scrape Function Tests', () => {
   for (let i = 0; i < argv.queries; i++) {
     it(`Test ${i + 1}: should successfully scrape data`, async function () {
-      this.timeout(argv.timeout + 1000); // Add an extra second to the timeout
+      this.timeout(argv.timeout + 1000);
 
       try {
         const productData = await scrape('1005005271402288', {
           reviewsCount: 20,
           rating: 2,
         });
-        // Ajoutez vos assertions ici selon les données attendues
         expect(productData).to.exist;
       } catch (error) {
-        // Si une erreur se produit, le test échoue
         throw new Error(`Test ${i + 1} failed: ${error.message}`);
       }
     });
